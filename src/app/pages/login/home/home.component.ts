4844e8a3abb9e6dbcc597dd44222268f623df8ce
Leonardo Frederiche <leofrederiche@gmail.com>
2023-02-23 16:40:22 -0300

    Post data to API & Layout

--- src/app/pages/login/home/home.component.ts
+++ src/app/pages/login/home/home.component.ts
@@ -1,7 +1,9 @@
 import { Component } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
+import { FormGroup, FormControl } from "@angular/forms";
+import * as moment from 'moment';
 
-import { EntryInterface } from "../../../interfaces/entry"
+import { EntryInterface, EntrieInterface } from "../../../interfaces/entry"
 
 @Component({
     selector: "app-home",
@@ -13,12 +15,20 @@ export class HomeComponent {
         private httpClient: HttpClient,
     ){}
 
-    ngOnInit() {
-        this.loadEntries()
-    }
-
     public entry = <EntryInterface>{
         totalBalance: "00:00"
+    }
+
+    public form: FormGroup = new FormGroup({
+        day: new FormControl(""),
+        in1: new FormControl(""),
+        out1: new FormControl(""),
+        in2: new FormControl(""),
+        out2: new FormControl("")
+    })
+
+    ngOnInit() {
+        this.loadEntries()
     }
 
     public loadEntries():void {
@@ -35,5 +45,34 @@ export class HomeComponent {
                 console.log("App Error -> ", error)
             }
         })
+    }
+
+    public saveEntry(): void {
+        const url = "http://lionic.com.br:3000/entry/register"
+        const user_id = "63169a0e1e03fa6c62cf165d"
+
+        const newEntry = <EntrieInterface>{}
+        newEntry.day = moment(this.form.value.day, 'DD/MM/YYYY').toDate()
+        newEntry.user = user_id
+
+        newEntry.entries = [this.form.value.in1, this.form.value.out1, this.form.value.in2, this.form.value.out2]
+
+        console.log("Salvando! ", newEntry)
+        this.httpClient.post<EntryInterface>(url, newEntry).subscribe({
+            next: data => {
+                this.loadEntries()
+            },
+            error: error => {
+                console.log("Error -> ", error)
+            }
+        })
+    }
+
+    public changeEntrie(entrie:EntrieInterface): void {
+        this.form.value.day = entrie.day
+        this.form.value.in1 = entrie.entries[0]
+        this.form.value.out1 = entrie.entries[1]
+        this.form.value.in2 = entrie.entries[2]
+        this.form.value.out2 = entrie.entries[3]
     }
 }
